c95b5fa4b263deb69784529dadb92070fdf88d5f
MD ISMAIL <smile@smile>
2024-06-24 18:09:04 +0600

    1.1 setup dataTable

---
+++ src/components/custom/DataTable.tsx
@@ -0,0 +1,52 @@
+'use client'
+import React, { useState } from 'react'
+import { Button } from '../ui/button'
+import {
+    ColumnDef,
+    ColumnFiltersState,
+    getFilteredRowModel,
+    flexRender,
+    getCoreRowModel,
+    useReactTable,
+    getPaginationRowModel,
+} from "@tanstack/react-table";
+import {
+    Table,
+    TableBody,
+    TableCell,
+    TableHead,
+    TableHeader,
+    TableRow,
+} from "@/components/ui/table";
+import { Input } from '../ui/input';
+
+interface DataTableProps<TData, TValue> {
+    columns: ColumnDef<TData, TValue>[];
+    data: TData[];
+    searchKey: string;
+}
+
+export function DataTable<TData, TValue>({
+    columns,
+    data,
+    searchKey
+}: DataTableProps<TData, TValue>) {
+    const [columnFilters, setColumnFilters] = useState<ColumnFiltersState>([])
+    const table = useReactTable({
+        data,
+        columns,
+        getCoreRowModel: getCoreRowModel(),
+        onColumnFiltersChange: setColumnFilters,
+        getPaginationRowModel: getPaginationRowModel(),
+        getFilteredRowModel: getFilteredRowModel(),
+        state: {
+            columnFilters
+        }
+    })
+    return (
+        <div>
+
+        </div>
+    )
+}
+
